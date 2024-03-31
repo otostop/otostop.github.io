418f86ae0d53d235706ca8a76c0bf0cfca6a3b80
kemalibircan <68851832+kemalibircan@users.noreply.github.com>
2024-03-31 11:54:15 +0300

    changes

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,11 @@
     $('body').vegas({
         slides: [
             { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/slide-image02.jpg' },
+            { src: 'images/slide-image03.jpg' },
+            { src: 'images/slide-image04.jpg' },
+            { src: 'images/slide-image05.jpg' },
+
         ],
         timer: false,
         transition: [ 'zoomOut', ]
